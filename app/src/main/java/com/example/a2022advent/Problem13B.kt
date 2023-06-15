444d9de8829d3be8ce47f4c4c295f02f700ac4f4
Cecilia Schmitz <ceciliamschmitz@gmail.com>
2023-06-15 16:16:23 -0500

    Problem 13B

--- app/src/main/java/com/example/a2022advent/Problem13B.kt
+++ app/src/main/java/com/example/a2022advent/Problem13B.kt
@@ -2,37 +2,56 @@ package com.example.a2022advent
 
 import java.io.File
 
-class Element(var isInt: Boolean, val num: Int, val list: ArrayList<Element>) {
-    override fun toString(): String {
-        return if (isInt) "$num" else "$list "
+class CompareElements : Comparator<Element> {
+    override fun compare(o1: Element?, o2: Element?): Int {
+        val left = copy(o1!!.list)
+        val right = copy(o2!!.list)
+        if (left.isEmpty() && right.isEmpty()) {
+            return 0
+        } else if (left.isEmpty()) {
+            return -1
+        } else if (right.isEmpty()) return 1
+        for (e in left.indices) {
+            if (e == right.size) return 1
+            if (left[e].isInt && right[e].isInt) {
+                if (left[e].num > right[e].num) return 1
+                if (left[e].num < right[e].num) return -1
+            } else {
+                if (left[e].isInt) {
+                    left[e].isInt = false
+                    left[e].list.add(Element(true, left[e].num, ArrayList()))
+                } else if (right[e].isInt) {
+                    right[e].isInt = false
+                    right[e].list.add(Element(true, right[e].num, ArrayList()))
+                }
+                var comp = compare(left[e], right[e])
+                if (comp == 1 || comp == -1) return comp
+            }
+        }
+        return if (right.size > left.size) {
+            -1
+        } else {
+            0
+        }
     }
 }
 
-class Problem13A
+class Problem13B
 
 fun main() {
     val filename = "/Users/cecilia.schmitz/AndroidStudioProjects/2022Advent/app/src/main/java/com/example/a2022advent/advent_13_input.txt"
-    var state = 0
-    var index = 1
-    var sum = 0
-    var left = ArrayList<Element>()
-    var right = ArrayList<Element>()
+    var sum = 1
+    var elements = ArrayList<Element>()
     File(filename).forEachLine {
-        when (state) {
-            0 -> left = parseInput(it)
-            1 -> right = parseInput(it)
-            2 -> {
-                if (checkElement(left, right) == 1) {
-                    sum += index
-                }
-                state = -1
-                index++
-            }
-        }
-        state++
+        if (it.isNotEmpty()) elements.add(Element(false, -1, parseInput(it)))
     }
-    if (checkElement(left, right) == 1) {
-        sum += index
+    val div1 = Element(false, -1, arrayListOf(Element(false, -1, arrayListOf(Element(true, 2, ArrayList())))))
+    val div2 = Element(false, -1, arrayListOf(Element(false, -1, arrayListOf(Element(true, 6, ArrayList())))))
+    elements.addAll(arrayOf(div1, div2))
+    elements.sortWith(CompareElements())
+    for (e in elements.indices) {
+        if (elements[e] == div1) sum *= e + 1
+        if (elements[e] == div2) sum *= e + 1
     }
     println(sum)
 }
@@ -105,4 +124,10 @@ private fun findMatchingBrace(index: Int, string: String): Int {
         if (matchingBraces == 0) return i
     }
     return -1
+}
+
+private fun copy(list: ArrayList<Element>): ArrayList<Element> {
+    val new = ArrayList<Element>()
+    for (e in list) new.add(Element(e.isInt, e.num, copy(e.list)))
+    return new
 }
